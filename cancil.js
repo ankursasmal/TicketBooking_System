a448716e09aceddeedf6ac8bd257897fea966cc4
ANKUR SASMAL <ankursasmal@ANKURs-MacBook-Air.local>
2024-02-09 18:50:51 +0530

    status

---
+++ cancil.js
@@ -0,0 +1,11 @@
+let cancil=document.querySelector('#cancil');
+ 
+let a= cancil.addEventListener('change',(e)=>{
+    console.log(e.target.value);
+  return e.target.value;
+ 
+})
+// export function onchang(){
+   
+// return a;
+// }
---
+++ cancil2.js
@@ -0,0 +1,5 @@
+let status=document.querySelector("#status");
+//  import { onchang } from "./cancil";
+//  let x=onchang();
+//  console.log(x);
+ status.innerHTML=`<p>your cancilation reason for ${a()} proceded</p>`
